943d46dc930ceddbd26c30a13ad743b448b39383
Noah <noah.verdeyen@gmail.com>
2023-01-10 17:09:36 -0500

    get injury report

--- build_db/get.py
+++ build_db/get.py
@@ -1,4 +1,11 @@
+import os.path
+
 import requests
+import time
+from sys import platform
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
 five_thirty_eight = "https://projects.fivethirtyeight.com/nba-model/2023/latest_RAPTOR_by_team.csv"
 req = requests.get(five_thirty_eight)
@@ -7,3 +14,21 @@ table = req.text
 data = open("player_stats.csv", "w")
 data.write(table)
 data.close()
+print(os.path.dirname(os.path.realpath(__file__)))
+options = Options()
+options.set_preference("browser.download.folderList", 2)
+options.set_preference("browser.download.manager.showWhenStarting", False)
+options.set_preference("browser.download.dir", os.path.dirname(os.path.realpath(__file__)))
+options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip")
+
+driver = webdriver.Firefox(options=options)
+
+injuries = "https://www.rotowire.com/basketball/injury-report.php"
+driver.get(injuries)
+
+time.sleep(3)
+button = driver.find_element(By.XPATH, '//button[@class="export-button is-csv"]')
+button.click()
+time.sleep(3)
+
+driver.close()
